92a305e392b5c4c5e4fa8dc60b341167915b7b79
Carlos <2414186+0xCaponte@users.noreply.github.com>
2023-08-25 21:31:04 +0200

    New Version Nr

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='image_format_converter',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'Pillow',
